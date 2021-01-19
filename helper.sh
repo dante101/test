d58b95ba9d01ea13f84d9e4bc5389398300e5db6
zerg <dante.101@inbox.ru>
2021-01-19 13:26:31 +0300

    update1

--- helper.sh
+++ helper.sh
@@ -1,10 +1,66 @@
 #!/bin/bash
 APACHE_BIN=/lib/systemd/system/apache2.service
-PG_BIN=`ls -l /var/lob/postgresql/`
-OS_VERSION=`cat /etc/astra_version`
+PG_BIN=$(ls -l /var/lob/postgresql/)
+OS_VERSION=$(cat /etc/astra_version)
 PID_PATH=/run/
 PG_V=""
-TIMESTAMP=`date "+%d-%m-%Y"`
+TIMESTAMP=$(date "+%d-%m-%Y")
+SITE15(){
+									touch /etc/apache2/site-avalable/$SITE_NAME 
+								        echo "
+									<VirtualHost *:$PORT>\n
+									ServerAdmin webmaster@localhost \n
+									DocumentRoot /var/www/$SITE_NAME \n
+									<Directory /> /n
+								 		AuthType Basic \n
+										AuthPAM_Enabled on \n
+										AuthName "PAM" \n
+										require valid-user \n
+										Options +Indexes +FollowSymLinks +MultiViews \n
+										AllowOverride None \n
+									</Directory> \n
+									 ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/ \n
+									<Directory "/usr/lib/cgi-bin"> \n
+										AllowOverride None \n
+										Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch \n
+										Order allow,deny \n
+										Allow from all \n
+									</Directory> \n
+									ErrorLog ${APACHE_LOG_DIR}/error.log	\n
+									LogLevel warn \n
+									CustomLog ${APACHE_LOG_DIR}/access.log combined \n
+									</VirtualHost> \n"  > /etc/apache2/site-avalable/$SITE_NAME 
+									 a2enmod $SITE_NAME 
+}
+site16 () {
+									touch /etc/apache2/site-avalable/$SITE_NAME.conf
+									echo  "<VirtualHost *:$PORT> \n
+									ServerAdmin webmaster@localhost \n
+									DocumentRoot /var/www/$SITE_NAME \n
+									<Directory /> \n
+								 		AuthType Basic \n
+										AuthPAM_Enabled on \n
+										AuthName "PAM" \n
+										require valid-user \n
+										Options +Indexes +FollowSymLinks +MultiViews \n
+										AllowOverride None \n
+									</Directory> \n
+									 ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/ \n
+									<Directory "/usr/lib/cgi-bin"> \n
+										AllowOverride None \n
+										Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch \n
+										Order allow,deny \n
+										Allow from all \n
+									</Directory> \n
+									ErrorLog ${APACHE_LOG_DIR}/error.log	\n
+									LogLevel warn \n
+									CustomLog ${APACHE_LOG_DIR}/access.log combined \n
+									</VirtualHost> \n " > /etc/apache2/site-avalable/$SITE_NAME.conf 
+									 a2enmod $SITE_NAME
+}
+	
+#create_db () {
+#createdb	
 #Проверка версии Астры
 if [[ "$OS_VERSION" = "SE 1.6 (smolensk)" ]]; then
 	OS_VERSION=16 
@@ -25,7 +81,8 @@ echo -e "\e[32mчто желаете хозяйн?\n
 \e[33mустановить сервер базы данны нажмите 2\e[0m\n
 \e[33mсделать резервную копию базы данных нажмите 3\e[0m\n 
 \e[33mудалить сервер базы данны нажмите 4\e[0m\n
-\e[33mудалить веб сервер apache2 нажмите 5\e[0m\n"
+\e[33mудалить веб сервер apache2 нажмите 5\e[0m\n
+\e[33mсоздать базу данных нажмите 6\e[0m\n"
 read VARIANT
 until [ -z $VARIANT  ]
 do
@@ -63,7 +120,7 @@ elif [[ -e $APACHE_BIN ]]; then
 				echo -e "\e[31mДа и не очень то и хотелось ..|.\e[0m"
 				exit 0
 				;;
-			[4-9,a-z,A-Z])
+						)
 				echo -e "WRONG ANSWER!!!"
 				exit 0
 				;;
@@ -78,22 +135,29 @@ setfacl -R -m u:www-data:r /etc/parsec/macdb
 setfacl  -m u:www-data:rx /etc/parsec/macdb
 read -p "Хотите создать конфигурационный файл?" ANS
 if [-z $ANS ]; then
+	echo -e "\e[31mне чего не выбрано.... exiting\e[0m"
 	exit 0
 elif
 	case $ANS in
 		[Д,д,Y,y,Yes,yes,Да,да] )
-			touch /etc/apache2/site-avalable/$ANS.conf
-			read -p "Укажите ПОРТ:" PORT
+			read -p "Имя сайта" SITE_NAME
+			read -p "Укажите ПОРТ. 80 или 8080 рекомендуется :" PORT
 				until [ -z $PORT ]; 
 				do
-				echo -e ""
-			echo "VirtualHost"
+				echo -e "не правельный порт"
+				if [[ $PORT != [1-65535] ]]; then
+				echo -e "\e[32не верное значение порта. порт должен быть в диапазоне 1 - 65535"
+				exit 1
+				elif 
+					if [[ OS_VERSION = 15 ]]; then site15
+					else  site16
+					fi
 				;;
 		[N,n,Н,н,Нет,нет,No,no] )
 				echo -e "\e[31mДа и не очень то и хотелось ..|.\e[0m"
 				exit 0
 				;;
-		[4-9,a-z,A-Z] )
+		   			)
 				echo -e "WRONG ANSWER!!!"
 				exit 0
 				;;
@@ -156,16 +220,17 @@ else
 fi
 '
 HOME=`pwd`
-psql -U postgres -l | cut -d \| -f 1 | egrep -v [^template]*,[^postgres]*
+SELECT_DB=$(psql -U postgres -l | cut -d \| -f 1 | egrep -v [^template]*,[^postgres]*)
+echo -e $SELECT_DB
 read -p "резервную копию какой базу данных выполнить: " DB_NAME
-DB_CHECK=`psql -U postgres -l | cut -d \| -f 1 | egrep $DB_NAME`
+DB_CHECK=$(psql -U postgres -l | cut -d \| -f 1 | egrep $DB_NAME)
 echo "$DB_CHECK"
 if [[ -z $DB_NAME ]]; then
 	echo -e "\e[31mне чего не выбрано\e[0m"
 	exit 0
 elif [[ "$DB_CHECK" != "$DB_NAME" ]]; then
 	echo -e "\e[31mнет такой базы данных\e[0m"
-	exit 1 >&2
+	exit 1
 elif [[ -e $HOME/$DB_NAME.tar.gz ]]; then
 	echo -e "\e[32mрезервная копия базы данных $DB_NAME уже существует . Удалить? Д/н\e[0m"
 		read DEL_DB
@@ -227,4 +292,11 @@ setfacl -R -m u:www-data:--- /etc/parsec/macdb
 setfacl  -m u:www-data:--- /etc/parsec/macdb
 ;;
 6)#Создание базы данных
+read -p "введите имя базы данных" NEW_DB
+DB_CHECK=$(psql -U postgres -l | cut -d \| -f 1 | egrep $DB_NAME)
+if [[ -z $NEW_DB ]];then echo -e  "\e[31mне чего не выбрано exiting..."
+	exit 0
+	elif [[ $DB_CHECK == $NEW_DB ]];then echo -e "\e[31$NEW_DB уже существует." 
+		exit 0
+	else	 
 esac
