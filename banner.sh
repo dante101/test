3066d88b1c88980f87a8897b9e2831cd48a02f79
zerg <dante.101@inbox.ru>
2021-01-22 15:42:29 +0300

    3

---
+++ banner.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#vars
+IP=$(ip a |egrep -v inet6 | egrep inet | awk '{print $2}' |egrep [0-9].[0-9].[0-9].[0-9] |egrep -v 127 |awk '{print $1}' | sort | uniq)
+RELEASE=$(cat /etc/os-release |egrep NAME | awk -F= '{print $2}' | egrep ^[a-z,A-Z,])
+KERNEL=$(uname -a | cut -d " " -f 3)
+USERS=$(who | cut -d' ' -f1 | sort | uniq)
+DATE=$(date)
+UPTIME=$(uptime |awk '{print $1,$2}')
+TOTAL_MEM=$(free -h | awk -c '{print $1,$2}' |egrep -i "mem:" |awk '{print $2} ')
+TOTAL_SWAP=$(free -h | awk -c '{print $1,$2}' |egrep -i "swap" |awk '{print $2}')
+USED_MEM=$(free -h | awk -c '{print $1,$3}' |egrep -i "mem" |awk '{print $2}')
+USED_SWAP=$(free -h | awk -c '{print $1,$2}' |egrep -i "swap" |awk '{print $2}')
+FREE_MEM=$(free -h | awk -c '{print $1,$4}' |egrep -i "mem" |awk '{print $2}')
+FREE_SWAP=$(free -h | awk -c '{print $1,$4}' |egrep -i "swap" |awk '{print $2}')
+LAST=$( last -15 |awk '{print $1,$3,$4,$5,$6,$7,$10}' |egrep -v reboot | egrep [0-9].[0-9].[0-9].[0-9] | sort )
+echo -e "\e[0m==========================================================================================================================="
+echo -e "\e[1;38;5;178mHi there !         			\e[1;5;38;5;208mWelcom to RUSBITECH SPB  Division\e[0m       "
+echo -e "\e[0m==========================================================================================================================="
+echo -e "\e[1;38;5;15m - Hostname .............................: $HOSTNAME  "
+echo -e "\e[1;38;5;15m - IP Address ...........................: $IP  "
+echo -e "\e[1;38;5;15m - Release ..............................: $RELEASE  "
+echo -e "\e[1;38;5;15m - Kernel ...............................: $KERNEL  "
+echo -e "\e[1;38;5;15m - Users who are login now...............: $USERS"
+echo -e "\e[1;38;5;15m - System current time ..................: $DATE  "
+echo -e "\e[1;38;5;15m - System uptime ........................: $UPTIME  "
+echo -e "\e[1;38;5;15m - Total Memory .........................: $TOTAL_MEM  "
+echo -e "\e[1;38;5;15m - SWAP .................................: $TOTAL_SWAP  "
+echo -e "\e[1;38;5;15m - Used Memory...........................: $USED_MEM  "
+echo -e "\e[1;38;5;15m - Used SWAP ............................: $USED_SWAP  "
+echo -e "\e[1;38;5;15m - Free Memory ..........................: $FREE_MEM  "
+echo -e "\e[1;38;5;15m - Free SWAP ............................: $FREE_SWAP  "
+echo -e "\e[0m=============================================================================================================================="
+echo -e "\e[1;38;5;15m - Last three logins were ...............: \n
+$LAST"
